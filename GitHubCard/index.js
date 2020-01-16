6ab31b4e4dd3a099864506da19940979859e2a92
eoinlynchcodes <eoin95lynch@gmail.com>
2020-01-16 21:57:39 +0000

    Finished. Happy with it.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,16 +3,16 @@
            https://api.github.com/users/<your name>
 */
 
-axios.get('https://api.github.com/users/eoinlynchcodes')
-.then(response => {
-  console.log(response);
-  let newCard = document.querySelector('.cards');
-  newCard.append(createComponent(response));
+// axios.get('https://api.github.com/users/eoinlynchcodes')
+// .then(response => {
+//   console.log(response);
+//   let newCard = document.querySelector('.cards');
+//   newCard.append(createComponent(response));
 
-})
-.catch(error => {
-  console.log(error);
-})
+// })
+// .catch(error => {
+//   console.log(error);
+// })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -35,7 +35,39 @@ axios.get('https://api.github.com/users/eoinlynchcodes')
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [ 'eoinlynchcodes', 'tetondan', 'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'
+];
+
+
+followersArray.forEach((item, key )=> {
+
+  axios.get(`https://api.github.com/users/${item}`)
+.then(response => {
+  console.log(response);
+  let newCard = document.querySelector('.cards');
+  newCard.append(createComponent(response));
+
+})
+.catch(error => {
+  console.log(error);
+})
+
+
+})
+// axios.get('https://api.github.com/users/')
+// .then(response => {
+//   console.log(response);
+//   let newCard = document.querySelector('.cards');
+//   newCard.append(createComponent(response));
+
+// })
+// .catch(error => {
+//   console.log(error);
+// })
+
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
