237c0507254743cd1916e1af744cebd701439db9
eoinlynchcodes <eoin95lynch@gmail.com>
2020-01-16 16:31:01 +0000

    Not finished. Restarted the project.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,14 @@
            https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/eoinlynchcodes')
+.then(response => {
+  console.log(response);
+})
+.catch(error => {
+  console.log(error);
+})
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -28,7 +36,6 @@ const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -43,8 +50,39 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
+
+function createComponent(){
+
+  // Create HTML Elements
+  const card = document.createElement('div');
+  const image = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const username = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const githubLink = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  // Structure. 
+
+  card.append(image);
+  cardInfo.append(name);
+  cardInfo.append(username);
+  cardInfo.append(location);
+  cardInfo.append(profile);
+  cardInfo.append(githubLink);
+  cardInfo.append();
+  cardInfo.append(name);
+  cardInfo.append(name);
+
+
+
+
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
